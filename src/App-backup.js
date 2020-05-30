161d9a1aa6810731a0fc6d3f24e5292aa8955937
= <=>
2020-05-31 05:26:58 +0530

    updated landing page

--- src/App-backup.js
+++ src/App-backup.js
@@ -3,120 +3,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let currentSet;
-let exportComp;
-
+// let currentSet;
+let setComponent;
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
-      cards: []
+      cards: [],
+      sets: [],
+      currentSet: null,
+      url : "https://cards-against-humanity-api.herokuapp.com/sets/"
     };
   }
-
-  componentDidMount() {
-    fetch("https://cards-against-humanity-api.herokuapp.com/sets/Base")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            cards: result
-          });
-          // console.log(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-  render()
+  dataGetter()
   {
-    let blacktext = "";
-    let warr = [];
-    let wcd;
-    let placed = false;
-    if (this.state.isLoaded === true){
-      placed = true;
-      let bcd = this.state.cards.blackCards; 
-      let rand = Math.floor(Math.random() * bcd.length);
-      blacktext = bcd[rand].text;
-      // console.log(rand);
-      // console.log(bcd[rand].text);
-      wcd = this.state.cards.whiteCards;
-      // console.log(wcd);
-
-      while (warr.length !== 6)
-      {
-        rand = Math.floor(Math.random() * wcd.length);
-        if (!warr.includes(rand))
-        {
-          warr.push(rand);
-        }
-      }
-    // console.log("The type: "+ typeof bcd[0])
-      
-    }
-
-    return ( 
-      <div className="App">
-        <div className="Tile">
-          <div class="Black-card">
-            {placed ? blacktext: 'Loading...'}
-          </div>
-        </div>
-        <div className="Tile">
-          <div class="White-card">
-            {placed ? wcd[warr[0]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-           {placed ? wcd[warr[1]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-            {placed ? wcd[warr[2]]: 'Loading...'}
-          </div>
-        </div>
-        <div className="Tile">
-          <div class="White-card">
-           {placed ? wcd[warr[3]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-           {placed ? wcd[warr[4]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-            {placed ? wcd[warr[5]]: 'Loading...'}
-          </div>
-        </div>
-      </div>
-
-    );
-  }
-}
-
-class Selection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      sets: []
-    };
-  }
-  SetNameSetting(setName)
-  {
-      currentSet = setName;
-      console.log(setName);
-  }
-  componentDidMount() {
-    fetch("https://cards-against-humanity-api.herokuapp.com/sets")
+    console.log(this.state.url)
+    console.log(this.state.currentSet)
+    if (this.state.url === "https://cards-against-humanity-api.herokuapp.com/sets/"){
+      fetch(this.state.url)
       .then(res => res.json())
       .then(
         (result) => {
@@ -136,38 +42,119 @@ class Selection extends React.Component {
           });
         }
       )
+    }
+    else{
+      fetch(this.state.url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            cards: result
+          });
+          // console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    } 
+  }
+  componentDidMount() {
+    this.dataGetter();
   }
   render()
   {
-    let blacktext = "";
-    let warr = [];
-    let wcd;
-    let placed = false;
-    let listItem;
-    if (this.state.isLoaded === true){
-      placed = true;
-      // console.log(this.state.sets[1].setName);
-      listItem = this.state.sets.map(
-        x => 
-        <div className="Set" key={x.setName} onClick={() => {currentSet = x.setName; console.log(currentSet);}}> {x.setName} </div>
-      ) 
+    if (this.state.currentSet){
+      let blacktext = "";
+      let warr = [];
+      let wcd;
+      let placed = false;
+  
+      if (this.state.isLoaded === true){
+        placed = true;
+        let bcd = this.state.cards.blackCards; 
+        let rand = Math.floor(Math.random() * bcd.length);
+        blacktext = bcd[rand].text;
+        // console.log(rand);
+        // console.log(bcd[rand].text);
+        wcd = this.state.cards.whiteCards;
+        // console.log(wcd);
+  
+        while (warr.length !== 6)
+        {
+          rand = Math.floor(Math.random() * wcd.length);
+          if (!warr.includes(rand))
+          {
+            warr.push(rand);
+          }
+        }
+      // console.log("The type: "+ typeof bcd[0])
+      }
+      setComponent = <div>
+        <div className="Tile">
+          <div class="Black-card">
+            {placed ? blacktext: 'Loading...'}
+          </div>
+        </div>
+        <div className="Tile">
+          <div class="White-card">
+            {placed ? wcd[warr[0]]: 'Loading...'}
+          </div>
+          <div class="White-card">
+          {placed ? wcd[warr[1]]: 'Loading...'}
+          </div>
+          <div class="White-card">
+            {placed ? wcd[warr[2]]: 'Loading...'}
+          </div>
+        </div>
+        <div className="Tile">
+          <div class="White-card">
+          {placed ? wcd[warr[3]]: 'Loading...'}
+          </div>
+          <div class="White-card">
+          {placed ? wcd[warr[4]]: 'Loading...'}
+          </div>
+          <div class="White-card">
+            {placed ? wcd[warr[5]]: 'Loading...'}
+          </div>
+        </div>
+    </div>;
     }
-
+    else{
+      let placed = false;
+      let listItem;
+      if (this.state.isLoaded === true){
+        placed = true;
+        // console.log(this.state.sets[1].setName);
+        listItem = this.state.sets.map(
+          x => 
+          <div className="Set" key={x.setName} onClick={() => {this.setState({
+            url : "https://cards-against-humanity-api.herokuapp.com/sets/"+x.setName,
+            isLoaded: false,
+            currentSet:  x.setName,
+            
+          }, () => {
+            console.log("Set name is :"+x.setName);
+            this.dataGetter();
+          }) ;
+            }}> {x.setName} </div>
+        ) 
+      }
+      setComponent = <div className="Tile">{listItem}</div>;
+    }
     return ( 
       <div className="App">
-      {/* <ul>{listItem}</ul> */}
-      <div className="Tile">
-         {listItem}
+        {setComponent}
       </div>
-      </div>
-
     );
   }
 }
-if (currentSet){
-  exportComp = App;
-}
-else{
-  exportComp =  Selection;
-}
-export default exportComp;
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,157 +1,37 @@
-import React from 'react';
-// import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { hot } from "react-hot-loader";
+import "./App.css";
 
-// let currentSet;
-let setComponent;
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      cards: [],
-      sets: [],
-      currentSet: null,
-      url : "https://cards-against-humanity-api.herokuapp.com/sets/"
-    };
-  }
-  dataGetter()
-  {
-    console.log(this.state.url)
-    console.log(this.state.currentSet)
-    if (this.state.url === "https://cards-against-humanity-api.herokuapp.com/sets/"){
-      fetch(this.state.url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            sets: result
-          });
-          // console.log(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-    }
-    else{
-      fetch(this.state.url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            cards: result
-          });
-          // console.log(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-    } 
-  }
-  componentDidMount() {
-    this.dataGetter();
-  }
-  render()
-  {
-    if (this.state.currentSet){
-      let blacktext = "";
-      let warr = [];
-      let wcd;
-      let placed = false;
-  
-      if (this.state.isLoaded === true){
-        placed = true;
-        let bcd = this.state.cards.blackCards; 
-        let rand = Math.floor(Math.random() * bcd.length);
-        blacktext = bcd[rand].text;
-        // console.log(rand);
-        // console.log(bcd[rand].text);
-        wcd = this.state.cards.whiteCards;
-        // console.log(wcd);
-  
-        while (warr.length !== 6)
-        {
-          rand = Math.floor(Math.random() * wcd.length);
-          if (!warr.includes(rand))
-          {
-            warr.push(rand);
-          }
-        }
-      // console.log("The type: "+ typeof bcd[0])
-      }
-      setComponent = <div>
-        <div className="Tile">
-          <div class="Black-card">
-            {placed ? blacktext: 'Loading...'}
-          </div>
+class App extends Component {
+  render() {
+    return (
+      <div className="buttons">
+        <label className = "welcome">
+          Welcome
+        </label>
+        <label className = "title">
+          Cards Against Humanity Online
+        </label>
+        
+        <div className = "buttons">
+        <button 
+          className = "join"
+          type = "submit"
+          variant="primary">
+          Join a room
+        </button>
+        <button 
+          className = "create"
+          type = "submit"
+          variant="primary">
+          Create a room
+        </button>
+        <form>
+        <input
+          type="text"
+          placeholder = "Username" />
+        </form>
         </div>
-        <div className="Tile">
-          <div class="White-card">
-            {placed ? wcd[warr[0]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-          {placed ? wcd[warr[1]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-            {placed ? wcd[warr[2]]: 'Loading...'}
-          </div>
-        </div>
-        <div className="Tile">
-          <div class="White-card">
-          {placed ? wcd[warr[3]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-          {placed ? wcd[warr[4]]: 'Loading...'}
-          </div>
-          <div class="White-card">
-            {placed ? wcd[warr[5]]: 'Loading...'}
-          </div>
-        </div>
-    </div>;
-    }
-    else{
-      let placed = false;
-      let listItem;
-      if (this.state.isLoaded === true){
-        placed = true;
-        // console.log(this.state.sets[1].setName);
-        listItem = this.state.sets.map(
-          x => 
-          <div className="Set" key={x.setName} onClick={() => {this.setState({
-            url : "https://cards-against-humanity-api.herokuapp.com/sets/"+x.setName,
-            isLoaded: false,
-            currentSet:  x.setName,
-            
-          }, () => {
-            console.log("Set name is :"+x.setName);
-            this.dataGetter();
-          }) ;
-            }}> {x.setName} </div>
-        ) 
-      }
-      setComponent = <div className="Tile">{listItem}</div>;
-    }
-    return ( 
-      <div className="App">
-        {setComponent}
       </div>
     );
   }
